9a49464fce21ca144b2bd99f8f3fc3aeef0b98eb
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2026-01-01 19:34:01 +0000

    added C00 ex05

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/01 19:20:21 by codespace         #+#    #+#             */
+/*   Updated: 2026/01/01 19:33:30 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_print_comb(void);
+
+/*
+int	main(void)
+{
+	ft_print_comb();
+}
+*/
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (a < '7')
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
